e36220bb1edc1c3ed2f168a9f735cd5fa3bf6075
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-06-21 11:03:23 -0700

    Formatted main.jsx with Prettier

--- student-store-ui/src/main.jsx
+++ student-store-ui/src/main.jsx
@@ -1,13 +1,13 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./components/App/App"
-import './globals.css'
-import { MantineProvider } from '@mantine/core';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App";
+import "./globals.css";
+import { MantineProvider } from "@mantine/core";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <MantineProvider withGlobalStyles withNormalizeCSS>
       <App />
     </MantineProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
