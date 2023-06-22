7eeb34508c33ac6a70426c93d5560ef47682c816
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-06-21 20:14:41 -0700

    Added props needed for ProductGrid to Home

--- student-store-ui/src/components/Home/Home.jsx
+++ student-store-ui/src/components/Home/Home.jsx
@@ -1,14 +1,22 @@
-import * as React from "react"
-import "./Home.css"
-import ProductGrid from "../ProductGrid/ProductGrid"
-import CategoryMenu from "../CategoryMenu/CategoryMenu"
+import * as React from "react";
+import "./Home.css";
+import ProductGrid from "../ProductGrid/ProductGrid";
+import CategoryMenu from "../CategoryMenu/CategoryMenu";
 
-export default function Home() {
+export default function Home({
+  products,
+  handleAddItemToCart,
+  handleRemoveItemToCart,
+}) {
   return (
     <div className="home">
       <p>Home</p>
       <CategoryMenu />
-      <ProductGrid />
+      <ProductGrid
+        products={products}
+        handleAddItemToCart={handleAddItemToCart}
+        handleRemoveItemToCart={handleRemoveItemToCart}
+      />
     </div>
-  )
+  );
 }
