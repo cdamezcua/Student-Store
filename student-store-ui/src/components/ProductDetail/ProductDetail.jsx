b8490a9d40e97380b9519274ac61462942832b30
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-06-22 12:48:58 -0700

    Added product fetch to ProductDetail

--- student-store-ui/src/components/ProductDetail/ProductDetail.jsx
+++ student-store-ui/src/components/ProductDetail/ProductDetail.jsx
@@ -1,15 +1,31 @@
 import * as React from "react";
 import "./ProductDetail.css";
+import { useState, useEffect } from "react";
 
 export default function ProductDetail({
   handleAddItemToCart,
   handleRemoveItemToCart,
 }) {
-  const [product, setProduct] = React.useState(null);
   const productId = window.location.pathname.split("/").pop();
+  const [product, setProduct] = React.useState(null);
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await fetch(
+          `https://codepath-store-api.herokuapp.com/store/${productId}`
+        );
+        const data = await response.json();
+        setProduct(data.product);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchProduct();
+  }, []);
+  console.log(product);
   return (
     <div className="product-detail">
-      <h1>Product details of product with id: {productId}</h1>
+      <h1>Hola</h1>
     </div>
   );
 }
