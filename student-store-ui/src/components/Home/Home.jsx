2ca93fa20d135a7e96e1a11e97138f68a25bc86f
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-06-22 01:06:54 -0700

    Added SearchBar to Home component

--- student-store-ui/src/components/Home/Home.jsx
+++ student-store-ui/src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import "./Home.css";
 import ProductGrid from "../ProductGrid/ProductGrid";
 import CategoryMenu from "../CategoryMenu/CategoryMenu";
+import SearchBar from "../SearchBar/SearchBar";
 import { useState } from "react";
 
 export default function Home({
@@ -10,19 +11,35 @@ export default function Home({
   handleRemoveItemToCart,
 }) {
   const [selectedCategory, setSelectedCategory] = useState("all-categories");
+  const [searchParameter, setSearchParameter] = useState("");
   return (
     <div className="home">
       <p>Home</p>
+      <SearchBar searchParameter={searchParameter} setSearchParameter={setSearchParameter} />
       <CategoryMenu
         selectedCategory={selectedCategory}
         setSelectedCategory={setSelectedCategory}
       />
       <ProductGrid
         products={products.filter((product) => {
-          if (selectedCategory === "all-categories") {
-            return true;
+          let meetsSearchCriteria = true;
+          if (searchParameter !== "") {
+            const searchParameterLower = searchParameter.toLowerCase();
+            const productNameLower = product.name.toLowerCase();
+            const productDescriptionLower = product.description.toLowerCase();
+            if (
+              !productNameLower.includes(searchParameterLower) &&
+              !productDescriptionLower.includes(searchParameterLower)
+            ) {
+              meetsSearchCriteria = false;
+            }
           }
-          return product.category === selectedCategory;
+          if (selectedCategory !== "all-categories") {
+            if (product.category !== selectedCategory) {
+              meetsSearchCriteria = false;
+            }
+          }
+          return meetsSearchCriteria;
         })}
         handleAddItemToCart={handleAddItemToCart}
         handleRemoveItemToCart={handleRemoveItemToCart}
---
+++ student-store-ui/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import "./SearchBar.css";
+import { TextInput } from "@mantine/core";
+
+export default function SearchBar({ searchParameter, setSearchParameter }) {
+  return (
+    <form className="search-bar">
+      <TextInput
+        placeholder="Search"
+        value={searchParameter}
+        onChange={(event) => {
+          setSearchParameter(event.target.value);
+        }}
+        onKeyDown={(event) => {
+          if (event.key === "Enter") {
+            event.preventDefault();
+          }
+        }}
+        inputStyle={{ textAlign: "end" }}
+      />
+    </form>
+  );
+}
