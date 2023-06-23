a4ed28d91977c0111a92845ced9a4d7fed193a5d
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-06-22 17:24:41 -0700

    Added style and links to Navbar

--- student-store-ui/src/components/Navbar/Navbar.jsx
+++ student-store-ui/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,19 @@
 import * as React from "react";
 import "./Navbar.css";
 import Logo from "../Logo/Logo";
+import { Group } from "@mantine/core";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <p>Navbar</p>
       <Logo />
+      <Group spacing="xl">
+        <Link to="/" className="link">Home</Link>
+        <Link to="/#about" className="link">About Us</Link>
+        <Link to="/#contact" className="link">Contact Us</Link>
+        <Link to="/#Buy" className="link">Buy Now</Link>
+      </Group>
     </nav>
   );
 }
